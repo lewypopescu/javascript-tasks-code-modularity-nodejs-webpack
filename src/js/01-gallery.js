12f9fa1d8debd0ae59a8a05bd24979a20cb1c033
lewypopescu <popesculewy@gmail.com>
2024-04-17 12:44:57 +0300

    start go

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryStart = document.querySelector('.gallery');
+
+function imagesCreate() {
+  for (const image of galleryItems) {
+    const imageMade = document.createElement('li');
+    imageMade.classList.add('gallery__item');
+    imageMade.innerHTML = `<a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>`;
+    galleryStart.append(imageMade);
+  }
+  const lightBox = new SimpleLightbox(`.gallery a`, {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+imagesCreate();
