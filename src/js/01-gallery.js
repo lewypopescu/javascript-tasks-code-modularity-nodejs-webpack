e8bb587a44f55fad1371992898a2d6fe67fc34b2
lewypopescu <popesculewy@gmail.com>
2024-04-18 12:15:23 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,17 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// Add imports above this line
-
 import { galleryItems } from './gallery-items';
 
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryStart = document.querySelector('.gallery');
 
 function imagesCreate() {
   for (const image of galleryItems) {
     const imageMade = document.createElement('li');
-    imageMade.classList.add('gallery__item');
-    imageMade.innerHTML = `<a class="gallery__link" href="${image.original}">
-      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    imageMade.style.listStyleType = 'none';
+    imageMade.innerHTML = `<a href="${image.original}">
+      <img src="${image.preview}" alt="${image.description}" />
    </a>`;
     galleryStart.append(imageMade);
   }
