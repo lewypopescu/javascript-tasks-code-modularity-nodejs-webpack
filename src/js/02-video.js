78eebde30da363d28a18e77a66c74b79783c2aa9
lewypopescu <popesculewy@gmail.com>
2024-04-19 11:29:46 +0300

    go go task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const vimeoPlayer = document.getElementById('vimeo-player');
+
+const playerV = new Player(vimeoPlayer);
+
+playerV.on(
+  'play',
+  throttle(function saveTime(e) {
+    e.preventDefault();
+    const currentT = () => {
+      localStorage.setItem(
+        'currentTime',
+        'playerV.getCurrentTime(data - seconds)'
+      );
+      const valueCT = localStorage.getItem('currentTime');
+      return valueCT;
+    };
+    const setCurrentT = playerV.setCurrentTime(currentT);
+    return setCurrentT;
+  }, 200)
+);
